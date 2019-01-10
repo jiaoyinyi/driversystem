39ab3d02cbdab910bc8e2627e976d3e17329e45e
jiaoyinyi <zaoyihuang@gmail.com>
2019-01-10 10:55:57 +0800

    fix some model define

--- model/health.go
+++ model/health.go
@@ -31,19 +31,19 @@ type Health struct {
 	Id     int     `json:"id" xorm:"pk autoincr"`
 	Sno    int     `json:"sno"`
 	Sname  string  `json:"sname"`
-	Height float64 `json:"height" xorm:"NULL"`
-	Weight float64 `json:"weight" xorm:"NULL"`
+	Height float64 `json:"height"`
+	Weight float64 `json:"weight"`
 	//辩色
-	Differentiate string  `json:"differentiate" xorm:"ENUM('正常','色弱','色盲') NULL"`
-	LeftSight     float64 `json:"left_sight" xorm:"NULL"`
-	RightSight    float64 `json:"right_sight" xorm:"NULL"`
-	LeftEar       string  `json:"left_ear" xorm:"ENUM('正常','偏弱') NULL"`
-	RightEar      string  `json:"right_ear" xorm:"ENUM('正常','偏弱') NULL"`
+	Differentiate string  `json:"differentiate" xorm:"ENUM('正常','色弱','色盲')"`
+	LeftSight     float64 `json:"left_sight"`
+	RightSight    float64 `json:"right_sight"`
+	LeftEar       string  `json:"left_ear" xorm:"ENUM('正常','偏弱')"`
+	RightEar      string  `json:"right_ear" xorm:"ENUM('正常','偏弱')"`
 	//腿长是否相等
-	Legs string `json:"legs" xorm:"ENUM('正常','不相等') NULL"`
+	Legs string `json:"legs" xorm:"ENUM('正常','不相等')"`
 	//血压
-	Pressure string `json:"pressure" xorm:"ENUM('正常','偏高','偏低') NULL"`
+	Pressure string `json:"pressure" xorm:"ENUM('正常','偏高','偏低')"`
 	//病史
-	History string `json:"history" xorm:"NULL"`
-	HText   string `json:"h_text" xorm:"TEXT NULL"`
+	History string `json:"history"`
+	HText   string `json:"h_text"`
 }
